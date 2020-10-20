bc40bfbb0c8d2ddcfaabdc8948ab60781285ef4b
Dhaval <dhavalpkakadiya@gmail.com>
2020-10-20 13:53:54 +0530

    => newsfeed image attachments type issue fix

--- app/screens/newsfeeds/NewsFeedList.js
+++ app/screens/newsfeeds/NewsFeedList.js
@@ -29,7 +29,7 @@ export default function NewsFeedList({ navigation, postData, userID }) {
         ListFooterComponent={() => (
             <View
             style={{
-              height: 20,
+              height: 70,
             }}
           />
         )}
