8ac9a431e233bf70491f2856307e371eef7bc7e6
manoj <patidarmanoj@gmail.com>
2020-10-20 20:43:44 -0700

    renamed enpoint

--- app/api/Reservationapi.js
+++ app/api/Reservationapi.js
@@ -1,5 +1,5 @@
 import Config from 'react-native-config';
-import api from '../utils/apiConstants';
+import api from '../utils/endPoints';
 import makeAPIRequest from '../utils/Global';
 
 // eslint-disable-next-line import/prefer-default-export
