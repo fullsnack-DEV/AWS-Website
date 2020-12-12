4393240763fbaf5fb2ee3cd2ba5a8e008a87db79
Kishan <makani20@gmail.com>
2020-12-12 16:29:34 +0530

    recorder scoreboard integrated

--- app/components/game/tennis/TennisScoreView.js
+++ app/components/game/tennis/TennisScoreView.js
@@ -1,45 +1,153 @@
-import React from 'react';
+/* eslint-disable array-callback-return */
+import React, { useEffect } from 'react';
 import {
   FlatList, Image, StyleSheet, Text, View,
 } from 'react-native';
 
-// import LinearGradient from 'react-native-linear-gradient';
+import LinearGradient from 'react-native-linear-gradient';
 import fonts from '../../../Constants/Fonts';
 import images from '../../../Constants/ImagePath';
 import TCThinDivider from '../../TCThinDivider';
 import colors from '../../../Constants/Colors';
 
+let setsData = [];
+const scoreData = [{}, {}, {}];
+let homeTeamGamePoint = '0';
+let awayTeamGamePoint = '0';
+let homeTeamMatchPoint = 0;
+let awayTeamMatchPoint = 0;
 export default function TennisScoreView({ scoreDataSource }) {
-  const renderScores = ({ item, index }) => (
+  useEffect(() => {
+    setsData = [];
+    const reversSets = scoreDataSource?.scoreboard?.sets.reverse();
+    for (let i = 0; i < scoreDataSource?.score_rules?.total_sets; i++) {
+      if (reversSets[i]) {
+        setsData.push(reversSets[i]);
+      } else {
+        setsData.push({});
+      }
+    }
+    calculateMatchScore();
+    calculateGameScore();
+    console.log('scoreDataSource', setsData);
+    // scoreDataSource?.scoreboard?.sets.reverse()
+    // scoreDataSource?.scoreboard?.sets?.length  < scoreDataSource?.score_rules?.total_sets
+    // setsData = scoreDataSource?.scoreboard?.sets?
+  }, []);
+
+  const renderScores = ({ item, index }) => (item.s_id === scoreDataSource?.scoreboard?.game_inprogress?.s_id ? (
+    <View style={{ alignItems: 'center' }}>
+      <Text style={styles.scoreTitle}>{index + 1}</Text>
+      <LinearGradient
+          colors={[colors.yellowColor, colors.themeColor]}
+          style={styles.scoreView}>
+        <Text style={[styles.player1Score, { color: colors.whiteColor }]}>
+          {item.s_id ? item.home_team_win_count : '-'}
+        </Text>
+        <TCThinDivider />
+        <Text style={[styles.player2Score, { color: colors.whiteColor }]}>
+          {item.s_id ? item.away_team_win_count : '-'}
+        </Text>
+      </LinearGradient>
+    </View>
+  ) : (
     <View style={{ alignItems: 'center' }}>
       <Text style={styles.scoreTitle}>{index + 1}</Text>
       <View style={styles.scoreView}>
-        <Text style={styles.player1Score}>{item[1]}</Text>
+        <Text style={styles.player1Score}>
+          {item.s_id ? item.home_team_win_count : '-'}
+        </Text>
         <TCThinDivider />
-        <Text style={styles.player2Score}>{item[2]}</Text>
+        <Text style={styles.player2Score}>
+          {item.s_id ? item.away_team_win_count : '-'}
+        </Text>
       </View>
     </View>
-    // <View style={{ alignItems: 'center' }}>
-    //   <Text style={styles.scoreTitle}>{index + 1}</Text>
-    //   <LinearGradient
-    //     colors={[colors.yellowColor, colors.themeColor]}
-    //     style={styles.scoreView}>
-    //     <Text style={[styles.player1Score, { color: colors.whiteColor }]}>
-    //       {item[1]}
-    //     </Text>
-    //     <TCThinDivider />
-    //     <Text style={[styles.player2Score, { color: colors.whiteColor }]}>
-    //       {item[2]}
-    //     </Text>
-    //   </LinearGradient>
-    // </View>
+  ));
+
+  const renderCurruentScores = ({ index }) => (
+    <View style={{ alignItems: 'center' }}>
+      <Text style={styles.scoreTitle}>
+        {(index === 0 && 'Sets')
+          || (index === 1 && 'Games')
+          || (index === 2 && 'Points')}
+      </Text>
+      <View style={styles.scoreView}>
+        <Text
+          style={
+            (index === 0 && homeTeamMatchPoint > awayTeamMatchPoint)
+            || (index === 1
+              && scoreDataSource?.scoreboard?.sets[0].home_team_win_count
+                > scoreDataSource?.scoreboard?.sets[0].away_team_win_count)
+            || (index === 2 && homeTeamGamePoint > awayTeamGamePoint)
+              ? [styles.player1Score, { color: colors.themeColor }]
+              : styles.player1Score
+          }>
+          {(index === 0 && `${homeTeamMatchPoint}`)
+            || (index === 1
+              && `${scoreDataSource?.scoreboard?.sets[0].home_team_win_count}`)
+            || (index === 2 && `${homeTeamGamePoint}`)}
+        </Text>
+        <TCThinDivider />
+        <Text
+          style={
+            (index === 0 && homeTeamMatchPoint < awayTeamMatchPoint)
+            || (index === 1
+              && scoreDataSource?.scoreboard?.sets[0].home_team_win_count
+                < scoreDataSource?.scoreboard?.sets[0].away_team_win_count)
+            || (index === 2 && homeTeamGamePoint < awayTeamGamePoint)
+              ? [styles.player2Score, { color: colors.themeColor }]
+              : styles.player2Score
+          }>
+          {(index === 0 && `${awayTeamMatchPoint}`)
+            || (index === 1
+              && `${scoreDataSource?.scoreboard?.sets[0].away_team_win_count}`)
+            || (index === 2 && `${awayTeamGamePoint}`)}
+        </Text>
+      </View>
+    </View>
   );
+
+  const calculateMatchScore = () => {
+    // eslint-disable-next-line no-unused-expressions
+    scoreDataSource?.scoreboard?.sets.map((e) => {
+      if (e.winner) {
+        if (e.winner === scoreDataSource.home_team.user_id) {
+          homeTeamMatchPoint += 1;
+        } else {
+          awayTeamMatchPoint += 1;
+        }
+      }
+    });
+  };
+  const calculateGameScore = () => {
+    // eslint-disable-next-line array-callback-return
+    if (
+      scoreDataSource?.scoreboard?.game_inprogress?.winner
+      || scoreDataSource?.scoreboard?.game_inprogress?.end_datetime
+    ) {
+      homeTeamGamePoint = '0';
+      awayTeamGamePoint = '0';
+
+      console.log(
+        'GAME SCORE:',
+        `HOME:${homeTeamGamePoint}AWAY:${awayTeamGamePoint}`,
+      );
+    } else {
+      homeTeamGamePoint = scoreDataSource?.scoreboard?.game_inprogress?.home_team_point;
+      awayTeamGamePoint = scoreDataSource?.scoreboard?.game_inprogress?.away_team_point;
+      console.log(
+        'GAME SCORE:',
+        `HOME:${homeTeamGamePoint}AWAY:${awayTeamGamePoint}`,
+      );
+    }
+  };
 
   return (
     <View style={styles.scoreContainer}>
       <View style={styles.leftScoreView}>
         <FlatList
-          data={scoreDataSource}
+          data={setsData}
           renderItem={renderScores}
           horizontal={true}
           showsHorizontalScrollIndicator={false}
@@ -54,19 +162,19 @@ export default function TennisScoreView({ scoreDataSource }) {
         <Text style={styles.centerTitle}>Player</Text>
         <View style={styles.centerScoreView}>
           <Image
-            source={images.profilePlaceHolder}
+            source={scoreDataSource?.home_team?.thumbnail ? { uri: scoreDataSource?.home_team?.thumbnail } : images.profilePlaceHolder}
             style={styles.player1Image}
           />
           <Image
-            source={images.profilePlaceHolder}
+            source={scoreDataSource?.away_team?.thumbnail ? { uri: scoreDataSource?.away_team?.thumbnail } : images.profilePlaceHolder}
             style={styles.player2Image}
           />
         </View>
       </View>
       <View style={{ width: '44%', height: 112 }}>
         <FlatList
-          data={scoreDataSource}
-          renderItem={renderScores}
+          data={scoreData}
+          renderItem={renderCurruentScores}
           horizontal={true}
           showsHorizontalScrollIndicator={false}
           ItemSeparatorComponent={() => (
@@ -115,11 +223,13 @@ const styles = StyleSheet.create({
     height: 27,
     width: 27,
     marginTop: 10,
+    borderRadius: 14,
   },
   player2Image: {
     height: 27,
     width: 27,
     marginBottom: 10,
+    borderRadius: 14,
   },
 
   scoreTitle: {
