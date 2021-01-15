df1ca0295c632868852fec875ef84c3dc3f8e6e1
Kishan <makani20@gmail.com>
2021-01-15 18:26:35 +0530

    github issue

--- ios/townscup_reactnative/AppDelegate.m
+++ ios/townscup_reactnative/AppDelegate.m
@@ -34,6 +34,11 @@ static void InitializeFlipper(UIApplication *application) {
   InitializeFlipper(application);
 #endif
   [FIRApp configure];
+  
+  if (@available(iOS 13.4, *)) {
+      [UIDatePicker appearance].preferredDatePickerStyle = UIDatePickerStyleWheels;
+    }
+  
   RCTBridge *bridge = [[RCTBridge alloc] initWithDelegate:self launchOptions:launchOptions];
   RCTRootView *rootView = [[RCTRootView alloc] initWithBridge:bridge
                                                    moduleName:@"townscup_reactnative"
