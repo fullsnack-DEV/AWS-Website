36a9fcbc775d7429276bc91aa7c599631ea34ea2
Kishan <makani20@gmail.com>
2021-02-26 17:16:18 +0530

    image compression, github issue and app performance

--- app/utils/imageAction.js
+++ app/utils/imageAction.js
@@ -1,6 +1,7 @@
 import fs from 'react-native-fs';
 import axios from 'axios';
 import { decode as atob } from 'base-64';
+import ImageResizer from 'react-native-image-resizer';
 import getImagePreSignedURL from '../api/Media';
 
 const base64ToArrayBuffer = (base64) => {
@@ -14,36 +15,109 @@ const base64ToArrayBuffer = (base64) => {
   // return Buffer.from(binaryString)
 };
 
+const T_LANDSCAPE_IMAGE_HEIGHT = 400;
+const T_PORTRAIT_IMAGE_WIDTH = 400;
+
+const O_LANDSCAPE_IMAGE_HEIGHT = 400;
+const O_PORTRAIT_IMAGE_WIDTH = 400;
 export const uploadImageOnPreSignedUrls = async ({
   url, uri, type, cancelToken,
 }) => {
-  const base64 = await fs.readFile(uri, 'base64');
-  const imgBuffer = base64ToArrayBuffer(base64);
-  const options = {
-    method: 'PUT',
-    url,
-    timeout: 180000,
-    headers: {
-      'Content-Type': type,
-      // 'Content-Type': `image/${type}`,
-      'Content-Length': imgBuffer.length,
-    },
-    data: imgBuffer,
-    cancelToken,
-  };
-  const response = await axios(options);
-  if (response.status === 200) {
-    return url.split('?')[0]
-  }
+ console.log('URL::=>', url);
+ console.log('URI::=>', uri);
+ console.log('Type::=>', type);
+
+   const base64 = await fs.readFile(uri, 'base64');
+   const imgBuffer = base64ToArrayBuffer(base64);
+
+    const options = {
+      method: 'PUT',
+      url,
+      timeout: 180000,
+      headers: {
+        'Content-Type': type,
+        // 'Content-Type': `image/${type}`,
+        'Content-Length': imgBuffer.length,
+      },
+      data: imgBuffer,
+      cancelToken,
+    };
+    const response = await axios(options);
+    if (response.status === 200) {
+      return url.split('?')[0]
+    }
+
   throw new Error('upoad-failed')
 };
 
-const uploadImage = (data, authContext, cancelToken) => {
+const thumbnailImageSize = (imageData) => {
+  console.log(`imageData width  ::${imageData.width} imageData height  ::${imageData.height}`);
+  const img = {};
+  if (imageData.height < imageData.width) {
+    if (imageData.height > T_LANDSCAPE_IMAGE_HEIGHT) {
+      img.height = T_LANDSCAPE_IMAGE_HEIGHT
+      img.width = parseInt((imageData.width / imageData.height) * T_LANDSCAPE_IMAGE_HEIGHT, 10)
+    } else {
+      img.height = imageData.height
+      img.width = imageData.width
+    }
+    console.log(`image width  ::${img.width} image height  ::${img.height}`);
+      return img
+  }
+    if (imageData.width > T_PORTRAIT_IMAGE_WIDTH) {
+      img.height = parseInt((imageData.height / imageData.width) * T_PORTRAIT_IMAGE_WIDTH, 10)
+      img.width = T_PORTRAIT_IMAGE_WIDTH
+    } else {
+      img.height = imageData.height
+      img.width = imageData.width
+    }
+    console.log(`image width  ::${img.width} image height  ::${img.height}`);
+      return img
+
+  // const heightRatio = imageData.height > 400 ?
+}
+const originalImageSize = (imageData) => {
+  console.log(`imageData width  ::${imageData.width} imageData height  ::${imageData.height}`);
+  const img = {};
+  if (imageData.height < imageData.width) {
+    if (imageData.height > O_LANDSCAPE_IMAGE_HEIGHT) {
+      img.height = O_LANDSCAPE_IMAGE_HEIGHT
+      img.width = parseInt((imageData.width / imageData.height) * O_LANDSCAPE_IMAGE_HEIGHT, 10)
+    } else {
+      img.height = imageData.height
+      img.width = imageData.width
+    }
+    console.log(`image width  ::${img.width} image height  ::${img.height}`);
+      return img
+  }
+    if (imageData.width > O_PORTRAIT_IMAGE_WIDTH) {
+      img.height = parseInt((imageData.height / imageData.width) * O_PORTRAIT_IMAGE_WIDTH, 10)
+      img.width = O_PORTRAIT_IMAGE_WIDTH
+    } else {
+      img.height = imageData.height
+      img.width = imageData.width
+    }
+    console.log(`image width  ::${img.width} image height  ::${img.height}`);
+      return img
+
+  // const heightRatio = imageData.height > 400 ?
+}
+const uploadImage = async (data, authContext, cancelToken) => {
   const image = {
     ...data,
     thumbURL: '',
     originalURL: '',
   };
+  let resThumb;
+  let resOriginal
+  if (image?.mime?.split('/')?.[0] === 'image') {
+   const thumbImgData = thumbnailImageSize(image)
+   const originalImgData = originalImageSize(image)
+
+     resThumb = await ImageResizer.createResizedImage(image.path, thumbImgData.width, thumbImgData.height, 'JPEG', 60, 0, null)
+     resOriginal = await ImageResizer.createResizedImage(image.path, originalImgData.width, originalImgData.height, 'JPEG', 72, 0, null)
+  }
+
   return getImagePreSignedURL({
     count: 2,
   }, authContext, cancelToken).then((response) => {
@@ -54,13 +128,13 @@ const uploadImage = (data, authContext, cancelToken) => {
     return Promise.all([
       uploadImageOnPreSignedUrls({
         url: preSignedUrls[0],
-        uri: image.path,
+        uri: image?.mime?.split('/')?.[0] === 'image' ? resOriginal.uri : image.path,
         type: image.mime,
         cancelToken,
       }),
       uploadImageOnPreSignedUrls({
         url: preSignedUrls[1],
-        uri: image.path,
+        uri: image?.mime?.split('/')?.[0] === 'image' ? resThumb.uri : image.path,
         type: image.mime,
         cancelToken,
       }),
