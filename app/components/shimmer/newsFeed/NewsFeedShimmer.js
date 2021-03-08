3b4fdc07dbf2247ad9b4c9381d9988e35673b974
rajkapoordev <rajkapoordev@gmail.com>
2021-03-08 11:13:52 +0530

    Conflicts And Shimmer

---
+++ app/components/shimmer/newsFeed/NewsFeedShimmer.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { View, ScrollView } from 'react-native';
+import { ShimmerView } from '../ShimmerCommonComponents';
+
+const NewsFeedShimmer = () => (
+  <ScrollView style={{ padding: 15, width: '100%' }}>
+    {Array(5)
+            .fill('')
+            .map((item, index) => (
+              <View key={index}>
+                <View key={index} style={{ flexDirection: 'row', marginBottom: 10 }}>
+                  <ShimmerView style={{ marginRight: 15, borderRadius: 50 }} width={30} height={30} />
+                  <View style={{ flex: 1 }}>
+                    <ShimmerView style={{ width: '60%', height: 10 }} />
+                    <ShimmerView style={{ width: '40%', height: 10 }} />
+                  </View>
+                </View>
+                <View key={index} style={{ marginBottom: 10 }}>
+                  <ShimmerView style={{ width: '100%' }} height={200} />
+                </View>
+
+                <View key={index} style={{ marginBottom: 10 }}>
+                  <ShimmerView style={{ width: '100%' }} height={15} />
+                  <ShimmerView style={{ width: '100%' }} height={15} />
+                  <ShimmerView style={{ width: '100%' }} height={5} />
+                </View>
+
+              </View>
+            ))}
+  </ScrollView>
+);
+
+export default NewsFeedShimmer;
