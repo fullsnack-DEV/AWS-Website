fc8fa9cf23119373859b0314872337acd66074f3
arvind patidar <arvindpatidar@arvinds-MacBook-Pro-2.local>
2023-01-19 02:03:53 +0530

    Universal search work

--- app/components/TCTeamSearchView.js
+++ app/components/TCTeamSearchView.js
@@ -5,46 +5,60 @@ import images from '../Constants/ImagePath';
 import colors from '../Constants/Colors';
 import fonts from '../Constants/Fonts';
 
-function TCTeamSearchView({onPress, showStar = false, data}) {
+function TCTeamSearchView({onPress, showStar = false, data, isClub = false}) {
   let teamIcon = '';
   let teamImagePH = '';
   if (data.entity_type === 'team') {
-    teamIcon = images.myTeams;
-    teamImagePH = images.team_ph;
+    teamIcon = images.newTeamIcon;
+    teamImagePH = images.teamPlaceholder;
   } else if (data.entity_type === 'club') {
-    teamIcon = images.myClubs;
-    teamImagePH = images.club_ph;
+    teamIcon = images.newClubIcon;
+    teamImagePH = images.clubPlaceholder;
   } else if (data.entity_type === 'league') {
     teamIcon = images.myLeagues;
     teamImagePH = images.leaguePlaceholder;
   }
-
   return (
     <TouchableOpacity onPress={onPress}>
       <View style={styles.viewContainer}>
-        <View style={styles.backgroundView}>
+        <View style={styles.placeholderView}>
           <Image
-            source={data?.thumbnail ? {uri: data?.thumbnail} : teamImagePH}
+            source={data.thumbnail ? {uri: data.thumbnail} : teamImagePH}
             style={styles.profileImage}
           />
-        </View>
-        <View style={{flexDirection: 'column', marginLeft: 5}}>
-          <View style={{flexDirection: 'row'}}>
-            <Text style={styles.entityName} numberOfLines={2}>
-              {data?.group_name}
+          {data.thumbnail ? null : (
+            <Text style={styles.oneCharacterText}>
+              {data.group_name?.charAt(0).toUpperCase()}
             </Text>
-            <Image
-              source={teamIcon}
-              style={styles.teamIconStyle}
-              resizeMode={'contain'}
-            />
-          </View>
-          <Text style={styles.locationText} numberOfLines={1}>
-            {data?.city} · {data?.sport?.toString()}
+          )}
+        </View>
+        <Image
+          source={teamIcon}
+          style={styles.teamIconStyle}
+          resizeMode={'contain'}
+        />
+        <View
+          style={{
+            flexDirection: 'column',
+            marginLeft: 10,
+          }}>
+          <Text style={styles.entityName} numberOfLines={2}>
+            {data.group_name}
           </Text>
+
+          {isClub ? (
+            <Text style={styles.locationText} numberOfLines={1}>
+              {data.city} · {data.sports.map((value) => value.sport).join(',')}
+            </Text>
+          ) : (
+            <Text style={styles.locationText} numberOfLines={1}>
+              {data.city} · {data.sport?.toString()}
+            </Text>
+          )}
+
           {showStar && (
             <Text style={styles.starPoints} numberOfLines={1}>
-              ★ 5{' '}
+              LV 13{' '}
               {data.length === 1 && data?.[0]?.setting
                 ? ` · ${data?.[0]?.setting?.game_fee?.fee} CAD`
                 : ''}
@@ -57,55 +71,54 @@ function TCTeamSearchView({onPress, showStar = false, data}) {
 }
 
 const styles = StyleSheet.create({
-  backgroundView: {
-    backgroundColor: colors.whiteColor,
-    elevation: 5,
-    height: 40,
-    width: 40,
-    borderRadius: 80,
-    shadowColor: colors.googleColor,
-    shadowOffset: {width: 0, height: 5},
-    shadowOpacity: 0.2,
-    shadowRadius: 5,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
   viewContainer: {
     flexDirection: 'row',
-    // alignItems: 'center',
-    // backgroundColor: 'red',
-    // height: 125,
   },
   profileImage: {
     resizeMode: 'cover',
-    height: 36,
-    width: 36,
+    height: 40,
+    width: 40,
     borderRadius: 80,
-    shadowColor: colors.googleColor,
-    shadowOffset: {width: 0, height: 5},
-    shadowOpacity: 0.2,
-    shadowRadius: 5,
+    marginLeft: 0,
   },
   entityName: {
     fontSize: 16,
-    fontFamily: fonts.RMedium,
+    fontFamily: fonts.RBold,
     color: colors.lightBlackColor,
   },
   locationText: {
     fontSize: 14,
     fontFamily: fonts.RRegular,
     color: colors.lightBlackColor,
+    marginTop: 1.5,
   },
 
   starPoints: {
     fontSize: 14,
     fontFamily: fonts.RRegular,
     color: colors.lightBlackColor,
+    marginTop: 1.5,
   },
   teamIconStyle: {
-    height: 25,
-    width: 25,
-    left: 2,
+    height: 15,
+    width: 15,
+    marginTop: 20,
+    marginLeft: -15,
+  },
+  oneCharacterText: {
+    position: 'absolute',
+    fontSize: 12,
+    fontFamily: fonts.RBlack,
+    color: colors.whiteColor,
+    paddingBottom: 5,
+  },
+  placeholderView: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginLeft: 5,
+    height: 40,
+    width: 40,
+    borderRadius: 80,
   },
 });
 
